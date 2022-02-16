9e27bc9280351c08b9e2b9ffc91d78dd2e6a94fd
xdze2 <xdze2.me@gmail.com>
2022-02-16 21:29:49 +0000

    Draft sn3218 driver

--- test_hardware/rgb_leds.py
+++ test_hardware/rgb_leds.py
@@ -1,65 +1,95 @@
-import sn3218 as sn
+
 import time
 
-# leds = sn.SN3218()
+from typing import List
 
-# print(sn.__version__)
-# enable_mask = 0b111111100011111111
-# leds.enable_leds(enable_mask)
-#
 
 import pigpio
 from smbus import SMBus
 
-UNDERLIGHTING_EN_PIN = 7
 
-gpio = pigpio.pi()
-gpio.set_mode(UNDERLIGHTING_EN_PIN, pigpio.OUTPUT)
-gpio.write(UNDERLIGHTING_EN_PIN, 1)
+class SN3218:
+
+    SHUTDOWN_REGISTER = 0x00
+    SET_PWM_REGISTER = 0x01
+    LED_CTRL_REGISTER = 0x13
+    UPDATE_REGISTER = 0x16
+    RESET_REGISTER = 0x17
+    
+    I2C_ADDRESS = 0x54
+
+    def __init__(self):
+        
+        self.UNDERLIGHTING_EN_PIN = 7
+
+        gpio = pigpio.pi()
+        gpio.set_mode(self.UNDERLIGHTING_EN_PIN, pigpio.OUTPUT)
+        gpio.write(self.UNDERLIGHTING_EN_PIN, 1)
+
+        self.i2c = SMBus(1)
+
+        self.start()
+        self.enable_all_leds()
+
+    # def hardware_shutdown(self):
+    #     gpio.write(self.UNDERLIGHTING_EN_PIN, 1)  
+
+    def _write(self, register_addr, data: List[int]):
+        self.i2c.write_i2c_block_data(self.I2C_ADDRESS, register_addr, data)
+
+    def start(self):
+        """Normal operation."""
+        self._write(self.SHUTDOWN_REGISTER, [0x01, ])
+
+    def shutdown(self):
+        """Software shutdown mode."""
+        self._write(self.SHUTDOWN_REGISTER, [0x00, ])
+
+    def _update(self):
+        """Update new state of Ctrl and PWM registers."""
+        self._write(self.UPDATE_REGISTER, [0xFF, ])
+
+    def enable_all_leds(self):
+        enable_mask = 0b111_111_111_111_111_111
+        self._write(
+            self.LED_CTRL_REGISTER,
+            [enable_mask & 0x3F, (enable_mask >> 6) & 0x3F, (enable_mask >> 12) & 0x3F],
+        )
+        self._update()    
+
+    def set_raw_pwm(self, values: List[int]):
+        self._write(
+            self.SET_PWM_REGISTER,
+            values
+        )
+        self._update()
 
 
-I2C_ADDRESS = 0x54
-CMD_ENABLE_OUTPUT = 0x00
-CMD_SET_PWM_VALUES = 0x01
-CMD_ENABLE_LEDS = 0x13
-CMD_UPDATE = 0x16
-CMD_RESET = 0x17
-
-
-i2c = SMBus(1)
-
-print(i2c)
-
-# enable
-i2c.write_i2c_block_data(I2C_ADDRESS, CMD_ENABLE_OUTPUT, [0x01])
-
-
-enable_mask = 0b111_111_111_111_111_111
-i2c.write_i2c_block_data(
-    I2C_ADDRESS,
-    CMD_ENABLE_LEDS,
-    [enable_mask & 0x3F, (enable_mask >> 6) & 0x3F, (enable_mask >> 12) & 0x3F],
-)
-time.sleep(1)
-i2c.write_i2c_block_data(I2C_ADDRESS, CMD_UPDATE, [0xFF])
-
+sn = SN3218()
 
 values = [
-    1, 0, 1,
-    1, 1, 0,
-    1, 0, 1,
-    1, 1, 0,
-    1, 0, 1,
-    1, 0,0 ,
+    250, 0, 250,
+    250, 250, 0,
+    250, 0, 250,
+    250, 250, 0,
+    250, 0, 250,
+    1, 250, 0,
 ]
 
+sn.set_raw_pwm(values)
 
-default_gamma_table = [int(pow(255, float(i - 1) / 255)) for i in range(256)]
-channel_gamma_table = [default_gamma_table for _ in range(18)]
+for k in range(10):
+    sn.start()
+    time.sleep(0.5)
+    sn.shutdown()
+    time.sleep(0.5)
+# default_gamma_table = [int(pow(255, float(i - 1) / 255)) for i in range(256)]
+# print(default_gamma_table)
+# channel_gamma_table = [default_gamma_table for _ in range(18)]
 
-i2c.write_i2c_block_data(I2C_ADDRESS, CMD_SET_PWM_VALUES, [channel_gamma_table[i][values[i]] for i in range(18)])
-i2c.write_i2c_block_data(I2C_ADDRESS, CMD_UPDATE, [0xFF])
+# i2c.write_i2c_block_data(I2C_ADDRESS, CMD_SET_PWM_VALUES, [channel_gamma_table[i][values[i]] for i in range(18)])
+# i2c.write_i2c_block_data(I2C_ADDRESS, CMD_UPDATE, [0xFF])
 
 
 
-time.sleep(1)
+# time.sleep(1)
