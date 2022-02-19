d5de304673f93378ce4add1d19073f6d89e65ca6
xdze2 <xdze2.me@gmail.com>
2022-02-19 22:54:41 +0100

    Typo

--- install.md
+++
@@ -1,6 +0,0 @@
-
-```
-# Matplotlib
-# ImportError: libopenjp2.so.7: cannot open shared object file: No such file or directory
-sudo apt-get install libopenjp2-7
-```
--- install.sh
+++ install.sh
@@ -3,13 +3,17 @@
 sudo apt install -y python3-venv
 sudo apt install libatlas3-base  # for numpy
 
-# pigiod
+# Matplotlib
+# ImportError: libopenjp2.so.7: cannot open shared object file: No such file or directory
+sudo apt-get install libopenjp2-7
+
+# pigiod
 sudo apt-get -y install pigpio python3-pigpio
 sudo pigpiod
 
+# Python venv
 python3 -m venv --system-site-packages venv 
 source venv/bin/activate
 
 pip install -e .
-
 pip install -r requirements.txt
