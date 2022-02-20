dcc223f51032227f17d830d8905c6c51e1362d20
xdze2 <xdze2.me@gmail.com>
2022-02-20 21:56:56 +0000

    Min working distance = front color

--- scripts/speed.py
+++ scripts/speed.py
@@ -9,6 +9,8 @@ import pigpio
 from pyrobot.drive import Mobility
 from pyrobot.hardware.sonar import UltraSoundSensor
 from pyrobot.gpio_mapping import SonarGpio
+from pyrobot.rgb_leds import RgbUnderlighting, HsvColor
+
 
 gpio = pigpio.pi()
 
@@ -19,27 +21,37 @@ mob = Mobility()
 
 
 
+leds = RgbUnderlighting()
+
+def distance_color_scale(distance: float):
+    h = int( distance/100 * 255 )
+    return HsvColor(min(255, max(h, 0)), 255, 255)
+
+
 data = []
 async def measure_distance():
-    for k in range(5):
+    while True:
         tic = time.perf_counter()
-        distance = (tic,  distance_sensor.measure_cm(nbr_measure=1))
+        distance = (tic, distance_sensor.measure_cm(nbr_measure=1))
         print(time.perf_counter() - tic)
+
         data.append(distance)
+
+        color = distance_color_scale(distance[1])
+        print(color, flush=True)
+        leds.change_color(color, 'front')
         await asyncio.sleep(0.001)
 
-
-def move():
-    mob.drive(50, 0, 1)
 
 
 async def main():
 
     task = asyncio.create_task(measure_distance())
     
-    mob.drive(80, 0)
-    await asyncio.sleep(3)
-    mob.stop()
+    # mob.drive(80, 0)
+    print('go')
+    await asyncio.sleep(10)
+    # mob.stop()
     # time.sleep(5)
     task.cancel()
 
