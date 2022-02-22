e5bece23eefa321d5f28acb7f83aa513e3db43d6
xdze2 <xdze2.me@gmail.com>
2022-02-22 21:29:19 +0000

    Draft rx lights

--- scripts/r.py
+++ scripts/r.py
@@ -1,52 +1,77 @@
 
-
-
-
 import time
-
 import asyncio
+
 import rx
 from rx.scheduler.eventloop import AsyncIOScheduler
-
 import rx.operators as op
-async_loop = asyncio.get_event_loop()
 
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import pigpio
+from pyrobot.drive import Mobility
+from pyrobot.hardware.sonar import UltraSoundSensor
+from pyrobot.gpio_mapping import SonarGpio
+from pyrobot.rgb_leds import RgbUnderlighting, HsvColor, ColorMap
+
+
+gpio = pigpio.pi()
+
+distance_sensor = UltraSoundSensor(gpio_echo=SonarGpio.ECHO, gpio_trig=SonarGpio.TRIG, gpio=gpio)
+
+
+mob = Mobility()
+
+
+cm = ColorMap(0, 1500, 'gist_heat')
+leds = RgbUnderlighting()
+
+
+async_loop = asyncio.get_event_loop()
 scheduler = AsyncIOScheduler(async_loop)
 
-# def gen():
-#     for k in range(10):
-#         time.sleep(0.5)
-#         yield k
 
-# test = rx.from_iterable(gen(), scheduler=scheduler)
+print('init done')
 
-test = rx.interval(1., scheduler=scheduler)
-
-print('done')
-test.subscribe(
-   lambda x: print("The value is {0}".format(x)),
-   on_error = lambda e: print("Error : {0}".format(e)),
-   on_completed = lambda: print("Job Done!"),
-   scheduler=scheduler
+distance_measure = (
+   rx.interval(0.100, scheduler=scheduler)
+      .pipe(
+         op.map( lambda t: (t, distance_sensor.measure_cm()) ),
+         op.scan( lambda acc, x: (x[0], 0.5*acc[1] + 0.5*x[1]) ) 
+      )
 )
-
-test.pipe(op.delay(1.4), op.map(lambda x:x**2)).subscribe(
-   lambda x: print("The square is {0}".format(x)),
-   on_error = lambda e: print("Error : {0}".format(e)),
-   on_completed = lambda: print("Job Done!"),
-   scheduler=scheduler
+(
+   distance_measure.pipe(
+      op.buffer_with_time(.5),
+      op.map(lambda grp: np.mean([u[1] for u in grp]))
+   ).subscribe(
+      lambda dist_avg: leds.change_color(cm.get_rgb(dist_avg), 'front'),
+      on_error = lambda e: print("Error : {0}".format(e)),
+      on_completed = lambda: print("Job Done!"),
+      scheduler=scheduler
+   )
 )
-
-
-test2 = rx.interval(3., scheduler=scheduler)
-
-print('done')
-test2.subscribe(
-   lambda x: print("The 2 value is {0}".format(x)),
-   on_error = lambda e: print("Error : {0}".format(e)),
-   on_completed = lambda: print("Job Done!"),
-   scheduler=scheduler
+distance_measure.subscribe(
+   lambda x: print(x),
 )
+# test.pipe(op.delay(1.4), op.map(lambda x:x**2)).subscribe(
+#    lambda x: print("The square is {0}".format(x)),
+#    on_error = lambda e: print("Error : {0}".format(e)),
+#    on_completed = lambda: print("Job Done!"),
+#    scheduler=scheduler
+# )
+
+
+# test2 = rx.interval(3., scheduler=scheduler)
+
+# print('done')
+# test2.subscribe(
+#    lambda x: print("The 2 value is {0}".format(x)),
+#    on_error = lambda e: print("Error : {0}".format(e)),
+#    on_completed = lambda: print("Job Done!"),
+#    scheduler=scheduler
+# )
 
 
 
@@ -54,4 +79,3 @@ test2.subscribe(
 async_loop.run_forever()
 print("done")
 async_loop.close()
-# input("Press Enter key to exit\n")
