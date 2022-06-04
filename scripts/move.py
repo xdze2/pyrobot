bca16bf5ddc5f8977f8a71c6331100a51c909c1a
xdze2 <xdze2@example.com>
2022-06-04 18:35:22 +0200

    Min working drive ctrl

--- scripts/move.py
+++ scripts/move.py
@@ -1,43 +1,58 @@
 
 
 import time
-
 from threading import Thread
 
-from pyrobot.drive import Mobility
-# from pyrobot.imu import Imu
-
-
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-
-
 import zmq
+from pyrobot.drive import Mobility
 
 
 # Prepare our context and publisher
 context = zmq.Context()
 subscriber = context.socket(zmq.PAIR)
+subscriber.setsockopt(zmq.LINGER, 0) 
 subscriber.bind("tcp://*:5564")
 # subscriber.setsockopt(zmq.SUBSCRIBE, b"key")
 
+
 mob = Mobility()
+mob.drive(0, 0) 
 while True:
      # Read envelope with address
-     key = subscriber.recv()
-     print('->', key)
+     state = subscriber.recv().decode('utf-8')
+     print('->', state)
 
-     if key == b'i':
-          print('forward')
-          mob.drive(70, 0)
-          time.sleep(.2)
-          mob.drive(0, 0)
-     elif key == b'k':
-          print('backward')
-          mob.drive(-70, 0)
-          time.sleep(.2)
-          mob.drive(0, 0)      
+     if 'i' in state:
+          speed = 70
+     elif 'k' in state:
+          speed = -70
+     else:
+          speed = 0
+
+
+     if 'j' in state:
+          curve = -50
+     elif 'l' in state:
+          curve = +50
+     else:
+          curve = 0
+     
+     print(f'speed:{speed}  curve:{curve}')
+     mob.drive(speed, curve)     
+        
+     # if key == b'i':
+     #      print('forward')
+     #      mob.drive(70, 0)
+     #      # time.sleep(.2)
+     #      # mob.drive(0, 0)
+     # elif key == b'k':
+     #      print('backward')
+     #      mob.drive(-70, 0)
+     #      time.sleep(.2)
+     #      mob.drive(0, 0)
+     # else:
+     #      print('stop')
+     #      mob.drive(0, 0)
      # [address, contents] = subscriber.recv()
      # print(f"[{address}] {contents}")
      
