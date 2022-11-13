3bfc64fe17f49842d13e2c8fd899534949b94aad
xdze2 <xdze2@example.com>
2022-11-13 19:47:43 +0100

    Mice odo1

---
+++ scripts/mice.py
@@ -0,0 +1,33 @@
+
+import struct 
+# https://thehackerdiary.wordpress.com/2017/04/21/exploring-devinput-1/
+
+
+
+# with open( "/dev/input/mice", "rb" ) as f:
+
+#     while True:
+#         data = f.read(10)
+#         print( struct.unpack('10b', data))
+
+
+# import struct
+# https://stackoverflow.com/a/12286738/8069403
+
+file = open( "/dev/input/mice", "rb" );
+
+def getMouseEvent():
+  buf = file.read(3)
+  print(buf)
+  button = ord( str(buf[0])[0] )
+  bLeft = button & 0x1
+  bMiddle = ( button & 0x4 ) > 0
+  bRight = ( button & 0x2 ) > 0
+  x,y = struct.unpack( "bb", buf[1:] )
+  print("L:%d, M: %d, R: %d, x: %d, y: %d" % (bLeft,bMiddle,bRight, x, y) )
+  # return stuffs
+
+while True:
+  getMouseEvent()
+
+file.close()
