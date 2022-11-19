bd0fb0641fca092dd7848f0e5ee56c1d296cd183
xdze2 <xdze2@example.com>
2022-11-19 18:48:44 +0100

    Integrate mice output

--- scripts/mice.py
+++ scripts/mice.py
@@ -15,17 +15,20 @@ import struct
 
 file = open("/dev/input/mice", "rb")
 
+x, y = 0, 0
 
 def getMouseEvent():
+    global x, y
     buf = file.read(3)
-    print(buf)
     button = ord(str(buf[0])[0])
     bLeft = button & 0x1
     bMiddle = (button & 0x4) > 0
     bRight = (button & 0x2) > 0
-    x, y = struct.unpack("bb", buf[1:])
-    print("L:%d, M: %d, R: %d, x: %d, y: %d" % (bLeft, bMiddle, bRight, x, y))
-    # return stuffs
+    dx, dy = struct.unpack("bb", buf[1:])
+    # print("L:%d, M: %d, R: %d, x: %d, y: %d" % (bLeft, bMiddle, bRight, dx, dy))
+    x += dx
+    y += dy
+    print(f"xy:\t{x: 5d}\t{y: 5d}")
 
 
 while True:
